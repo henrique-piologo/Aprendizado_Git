21f533ba9b5427c0eb6e0fd6fc968daf302f10ae
Gabriel-Nunes-lang <gabrielfernandescardena@gmail.com>
2025-03-17 20:55:56 -0300

    Adicionando classe Carro

--- TuturialGit.java
+++ TuturialGit.java
@@ -3,8 +3,9 @@ import entiny.Automovel;
 public class TuturialGit {
     public static void main(String[] args) {
         Automovel automovel = new Automovel("chevrolet", 2016, "Astra");
-        
+
         automovel.Ligar();
         automovel.Desligar();
+        automovel.Exibir();
     }
 }
