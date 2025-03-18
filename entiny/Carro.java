1d98b97d24ac2a694b8181e2dc96d04842a3491c
Gabriel-Nunes-lang <gabrielfernandescardena@gmail.com>
2025-03-17 21:10:54 -0300

    Adicionado o Carro

---
+++ entiny/Carro.java
@@ -0,0 +1,23 @@
+package entiny;
+
+public class Carro extends Automovel{
+    private int quantportas;
+    private String motor;
+    private boolean direcaohidraulica;
+
+    public Carro (int quantportas, String motor, boolean direcaohidraulica, String marca, int anoDeFabricacao, String modelo){
+        super (marca, anoDeFabricacao, modelo);
+        this.quantportas=quantportas;
+        this.motor=motor;
+        this.direcaohidraulica=direcaohidraulica;
+    }
+
+    public void TrancarPorta(){
+        System.out.println("Porta trancada");
+    }
+
+    public void LigarAlarme(){
+        System.out.println("Alarme ligado");
+    }
+}
+
